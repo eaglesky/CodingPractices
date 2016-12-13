e8bb00f95da399a9c699f65bccdd3b95bdfd2ead
Yalun Qin <yqin@rocketfuel.com>
2016-12-13 17:00:36 +0800

    Add several sorting algorithms

--- Sortings.java
+++ Sortings.java
@@ -81,10 +81,10 @@ public class Sortings {
 		System.out.println("After selection sort: " + Arrays.toString(nums));
 	}
 
-	//Merge sort.
-	//O(nlogn) time and O(nlogn) space
+	//Top-down merge sort.
+	//O(nlogn) time and O(n) space
 	//Stable
-	static int[] mergeSort(int[] nums, int start, int end) {
+	private static int[] mergeSort(int[] nums, int start, int end) {
 		if (start == end) {
 			return new int[]{nums[start]};
 		}
@@ -103,12 +103,122 @@ public class Sortings {
 		return sortedNums;
 	}
 
-	static void mergeSort(int[] nums) {
+	static void mergeSortTopDown(int[] nums) {
 		if (nums.length > 0) {
 			int[] sortedNums = mergeSort(nums, 0, nums.length - 1);
 			System.arraycopy(sortedNums, 0, nums, 0, sortedNums.length);
 		}
-		System.out.println("After merge sort: " + Arrays.toString(nums));
+		System.out.println("After top-down merge sort: " + Arrays.toString(nums));
+	}
+
+	//Bottome-up merge sort.
+	//Performance and stability are the same as the top-down one.
+	static void mergeSortBottomUp(int[] nums) {
+		int[] nums2 = Arrays.copyOf(nums, nums.length);
+		for(int k = 1; k < nums.length; k = k << 1) {
+			int[] temp = new int[nums2.length];
+			for (int leftStart = 0; leftStart < nums2.length;) {
+				int rightStart = leftStart + k;	
+				if (rightStart >= nums2.length) {
+					for (; leftStart < nums2.length; ++leftStart) {
+						temp[leftStart] = nums2[leftStart];
+					}
+				} else {
+					int rightEnd = Math.min(rightStart + k, nums2.length);
+					for(int i = leftStart, j = rightStart; i < rightStart || j < rightEnd;) {
+						if (i < rightStart && (j == rightEnd || nums2[i] <= nums2[j])) {
+							temp[leftStart++] = nums2[i++];
+						} else {
+							temp[leftStart++] = nums2[j++];
+						}
+					}
+				}
+			}
+			nums2 = temp;
+		}
+		System.arraycopy(nums2, 0, nums, 0, nums.length);
+		System.out.println("After bottom-up merge sort: " + Arrays.toString(nums));
+	}
+
+	private static Random rand = new Random();
+
+	private static void swap(int[] nums, int i, int j) {
+		int temp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = temp;
+	}
+
+	private static int partition(int[] nums, int start, int end) {
+		if (start >= end) {
+			return start;
+		}
+		int id = rand.nextInt(end - start + 1) + start;
+		int pivot = nums[id];
+		int i = start;
+		//Has to be i <= j rather than i < j, because it is possible that 
+		//i == j after swapping and changing i and j. When that happens, 
+		//nums[i] has to be checked again. nums[i] .. nums[end] must be guranteed 
+		//to be larger than nums[start]..num[i-1]. Therefore equals should be added
+		//to the break condition. And it should also be added to the condition of swap
+		//to prevent indefinite loop.
+		//E.g. nums = [2, 1, 2], and id = 2. 
+		for (int j = end; i <= j;) {
+			for (; nums[i] < pivot; ++i);
+			for (; nums[j] > pivot; --j);
+			if (i <= j) {
+				swap(nums, i, j);
+				++i;
+				--j;
+			}
+		}
+		return i;
+	}
+
+	//Quick sort
+	//Worst case time O(n^2). Average O(nlogn). O(n) space worst case. O(logn) space on average
+	//Unstable
+	private static void quickSort(int[] nums, int start, int end) {
+		if (start < end) {
+			int i = partition(nums, start, end);
+			quickSort(nums, start, i-1);
+			quickSort(nums, i, end);
+		}
+	}
+
+	static void quickSort(int[] nums) {
+		quickSort(nums, 0, nums.length - 1);
+		System.out.println("After quick sort: " + Arrays.toString(nums));
+	}
+
+	//Counting sort
+	//O(n + k) time and O(n + k) space. Works only when the input elements can be
+	//mapped to a range of integers [0, k-1].
+	//Stable!
+	static void countingSort(int[] nums) {
+		if (nums.length <= 1) {
+			return;
+		}
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		for(int num : nums) {
+			min = Math.min(num, min);
+			max = Math.max(num, max);
+		}
+		int[] counts = new int[max - min + 1];
+		for (int num : nums) {
+			counts[num - min]++;
+		}
+		for (int i = 1; i < counts.length; ++i) {
+			counts[i] += counts[i-1];
+		}
+		int[] sorted = new int[nums.length];
+		for (int i = nums.length - 1; i >= 0; --i) {
+			int countId = nums[i] - min;
+			sorted[counts[countId]-1] = nums[i];
+			counts[countId]--;
+		}
+		System.arraycopy(sorted, 0, nums, 0, nums.length);
+		System.out.println("After counting sort: " + Arrays.toString(nums));
 	}
 
 	public static void main(String[] args) {
@@ -117,7 +227,8 @@ public class Sortings {
 			{1},
 			{1, 3, 5, 7},
 			{9, 8, 3, 11, -55},
-			{4, 4, 8, 12, 55, 3, 8, -3, 4}
+			{4, 4, 8, 12, 55, 3, 8, -3, 4},
+			{2, 2, 3, 2, 1, 2}
 		};
 		for (int[] array : nums) {
 			System.out.println("Original array: " + Arrays.toString(array));
@@ -125,7 +236,10 @@ public class Sortings {
 			bubbleSort(Arrays.copyOf(array, array.length));
 			bubbleSortOptimized(Arrays.copyOf(array, array.length));
 			selectionSort(Arrays.copyOf(array, array.length));
-			mergeSort(Arrays.copyOf(array, array.length));
+			mergeSortTopDown(Arrays.copyOf(array, array.length));
+			mergeSortBottomUp(Arrays.copyOf(array, array.length));
+			quickSort(Arrays.copyOf(array, array.length));
+			countingSort(Arrays.copyOf(array, array.length));
 			System.out.println("");
 		}
 		
